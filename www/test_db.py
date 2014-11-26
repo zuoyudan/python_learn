7f1117beb6f421f71508bb65d176f9a67251bd0c
zuoyudan <zuoyudan@126.com>
2014-11-26 16:40:22 +0800

    test db

---
+++ www/test_db.py
@@ -0,0 +1,10 @@
+from models import User,Blog,Comment
+
+from transwarp import db
+
+db.create_engine(user='www-data',password='www-data',database='awesome')
+u=User(name='Test',email='test@example.com',password='123456',image='about:blank')
+u.insert()
+print 'new user id:',u.id
+u1=User.find_first('where email=?','test@example.com')
+print 'find user\'s name:',u1.name
