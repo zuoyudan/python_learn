83da59e724c8b13937265cbc9492f9c13302d5ce
zuoyudan <zuoyudan@126.com>
2014-11-26 21:03:41 +0800

    add file test_db.py

--- www/test_db.py
+++ www/test_db.py
@@ -8,3 +8,6 @@ u.insert()
 print 'new user id:',u.id
 u1=User.find_first('where email=?','test@example.com')
 print 'find user\'s name:',u1.name
+u1.delete()
+u2=User.find_first('where email=?','test@example.com')
+print 'find user:',u2
