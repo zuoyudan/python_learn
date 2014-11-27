753c6dd65c89bb01ca731024d3b4f347e57eaf80
zuoyudan <zuoyudan@126.com>
2014-11-27 11:16:45 +0800

    'before mvc'

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+__author__='wolf zoe'
+
+'''
+Default configurations
+'''
+
+configs={
+	'db':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'www-data',
+		'database':'awesome'
+	},
+	'session':{
+		'secret':'AsEsOmE'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__='wolf zoe'
+
+configs={
+	'db':{
+		'host':'127.0.0.1'
+	}
+}
